c8656c61d7e1b9108dbd29254dab56d1af5c58f4
Cesar Contreras <cesar.contrerasm@outlook.com>
2021-03-30 15:37:58 -0400

    Adding dummy data to display in the UI

---
+++ Weather/Models/WeatherInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Weather.Models
+{
+    public class WeatherInfo
+    {
+        public string City { get; set; }
+        public string CurrentIcon { get; set; }
+        public int CurrentTemp { get; set; }
+        public string TomorrowIcon { get; set; }
+        public int TomorrowTemp { get; set; }
+    }
+}
--- Weather/ViewModels/WeatherViewModel.cs
+++ Weather/ViewModels/WeatherViewModel.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Weather.Models;
+
 namespace Weather.ViewModels
 {
     public class WeatherViewModel : BaseViewModel
     {
-
+        private ObservableCollection<WeatherInfo> weather = new ObservableCollection<WeatherInfo>();
 
         public WeatherViewModel()
         {
             Title = "Weather";
+
+            Weather.Add(new WeatherInfo()
+            {
+                City = "Nutley",
+                CurrentIcon = "https://openweathermap.org/img/wn/10d@2x.png",
+                CurrentTemp = 27,
+                TomorrowIcon = "https://openweathermap.org/img/wn/10d@2x.png",
+                TomorrowTemp = 19
+            });
+            Weather.Add(new WeatherInfo()
+            {
+                City = "Passaic",
+                CurrentIcon = "https://openweathermap.org/img/wn/10d@2x.png",
+                CurrentTemp = 27,
+                TomorrowIcon = "https://openweathermap.org/img/wn/10d@2x.png",
+                TomorrowTemp = 19
+            });
         }
 
-
+        public ObservableCollection<WeatherInfo> Weather
+        {
+            get => weather;
+            set => weather = value;
+        }
     }
 }
